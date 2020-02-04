534b4e74743fc40eb34f2a485c1510b76e464242
Phil Hawker <philhawker1211@gmail.com>
2020-02-04 11:06:36 -0700

    inital commit

---
+++ compound-assignment.js
@@ -0,0 +1,20 @@
+
+var name = 'Kelsey';
+
+var sum = 0;
+
+var gradeOne = 100;
+
+var gradeTwo = 80;
+
+sum += gradeOne; // 100
+
+sum; // 100
+
+sum += gradeTwo; // 180
+
+sum *= gradeTwo; // 14400
+
+
+console.log(sum);
+
