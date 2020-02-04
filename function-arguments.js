d059e70e3ab174e3d9bfdecf1973b39ec80b17c0
Phil Hawker <philhawker1211@gmail.com>
2020-02-04 16:18:14 -0700

    inital commit

---
+++ function-arguments.js
@@ -0,0 +1,14 @@
+function fullName(firstName, lastName) {
+    return lastName.toUpperCase() + ", " + firstName.toUpperCase()
+}
+
+
+
+console.log(fullName('kelsey', 'hawker'))
+
+function name(firstName, lastName, language) {
+    language = language || 'English'                         // || means or. this line is setting a default value for the argument language
+    return lastName.toUpperCase() + ", " + firstName.toUpperCase() + ' - ' + language
+}
+
+console.log(name('kelsey', 'hawker'))
