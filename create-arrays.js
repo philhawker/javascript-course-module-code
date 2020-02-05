461c7084a7d566c56a8af2006283d52060eaf904
Phil Hawker <philhawker1211@gmail.com>
2020-02-05 11:44:35 -0700

    inital commit

---
+++ create-arrays.js
@@ -0,0 +1,9 @@
+var generatedArray = new Array(3); // [undefined, undefined, undefined]
+
+var literalArray = [1, 2, 3];
+
+var mixedArray = ['Hi', 1, ['a', 'b', 'c'], { name: 'Kristine' }];
+
+console.log(generatedArray)
+console.log(literalArray)
+console.log(mixedArray)
