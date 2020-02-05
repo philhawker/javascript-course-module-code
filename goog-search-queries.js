0c75ea91f48c091290f8fbc9a8a018103630f2a3
Phil Hawker <philhawker1211@gmail.com>
2020-02-05 15:01:53 -0700

    initial commit

---
+++ goog-search-queries.js
@@ -0,0 +1,10 @@
+// this is the way to call their new classNames;
+
+    //     $('.gLFyf')
+
+//  to perform a search
+
+    //    $(".FPdoLc").childNodes[1].childNodes[1].click()
+
+
+//   When creating a Script, the first step is to query the item in the DOM and place it into a variable 
