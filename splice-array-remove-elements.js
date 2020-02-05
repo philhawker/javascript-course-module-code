74bf21200c0fd90f66a45c7c8953df6257c52efb
Phil Hawker <philhawker1211@gmail.com>
2020-02-05 12:29:51 -0700

    inital commit

---
+++ splice-array-remove-elements.js
@@ -0,0 +1,15 @@
+var arr = ['Altuve', 'Bregman', 'Correa', 'Springer'];
+
+var foundElement = arr.indexOf('Correa');
+
+foundElement; // 2
+
+arr.splice(foundElement, 1); // ["Correa"]
+
+arr; // ["Altuve", "Bregman", "Springer"]
+
+arr.splice(1, 2); // ["Bregman", "Springer"]   // the second number specifies how many of that specific object should be removed
+
+arr; // ["Altuve"]
+
+console.log(foundElement)
